f07305bbd55bc1da71e1b8539d4f44d011045c01
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2023-05-05 23:39:13 +0300

    add default files

---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php
+    require_once( get_template_directory().'/functions-parts/breadcrumbs.php' );
+    require_once( get_template_directory().'/functions-parts/navmenu.php' );
+    require_once( get_template_directory().'/functions-parts/crb_init.php' );
