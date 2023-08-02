7f3493eb9a2d3a609035789608b1f8bc338109a0
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2023-08-02 22:08:16 +0300

    empty

--- woocommerce/wc-functions-remove.php
+++ woocommerce/wc-functions-remove.php
@@ -33,3 +33,4 @@ remove_action('woocommerce_single_product_summary', 'woocommerce_template_single
 
 remove_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 10 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+
