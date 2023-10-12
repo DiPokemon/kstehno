a4f283e12092f0970e42fdf36fc2a67166730b9b
Timur-tiger <timahusamov@mail.ru>
2023-10-12 11:52:33 +0300

    надпись в футере

--- footer.php
+++ footer.php
@@ -3,8 +3,13 @@
         <footer itemprop="mainContentOfPage" itemscope itemtype="https://schema.org/WPFooter">
             <?php include 'template-parts/variables.php' ?>
             <div class="container">
+            <p class="work_only">Работаем только с юридическими лицами и индивидуальными предпринимателями.</p>
                     <div class="footer_top">
+                    
                         <div class="footer_top_left">
+                       
+                       
+
                             <?php
                                 $args = array(
                                     'container'       => 'nav',          
