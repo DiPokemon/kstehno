649b04f228f62f73fd54232f187044288ac61f6b
Timur-tiger <timahusamov@mail.ru>
2023-11-28 21:07:38 +0300

    удаление wp-json

--- functions.php
+++ functions.php
@@ -156,3 +156,7 @@ function my_custom_mime_types( $mimes ) {
 	return $graphs;
 	}
 
+remove_action('wp_head', 'rest_output_link_wp_head', 10);
+remove_action('template_redirect', 'rest_output_link_header', 11);
+// Удаление ссылок на oembed
+remove_action('wp_head', 'wp_oembed_add_discovery_links');
