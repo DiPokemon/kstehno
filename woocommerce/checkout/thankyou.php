cb694f2434f3a510bd2af56a3555481fb76efc56
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2024-01-21 13:41:31 +0300

    thank you page

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -76,17 +76,10 @@ defined( 'ABSPATH' ) || exit;
                 </ul>
                 <?php do_action( 'woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id() ); ?>
             </div>
-
-			
-
 		<?php endif; ?>
 
-        <div class="half_column">
-            <?php do_action( 'woocommerce_thankyou', $order->get_id() ); ?>
-        </div>
-
-		
-		
+      
+        <?php do_action( 'woocommerce_thankyou', $order->get_id() ); ?>	
 
 	<?php else : ?>
 
