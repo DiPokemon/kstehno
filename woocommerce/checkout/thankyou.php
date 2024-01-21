34ffe0081265daa2a0e457e1a11e5f6f0072b323
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2024-01-21 13:52:32 +0300

    thankyou page

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -39,7 +39,7 @@ defined( 'ABSPATH' ) || exit;
 
 		<?php else : ?>
 
-            <div class="half_column">
+            <div>
                 <p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 
                 <ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
